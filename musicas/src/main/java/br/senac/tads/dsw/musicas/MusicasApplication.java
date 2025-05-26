f9614adafcae47dd256420fdcec3bc83034fb1a9
Vinicius Vieira <vvieira2045@gmail.com>
2025-05-26 19:40:32 -0300

    Adiciona arquivos extraídos do projeto de músicas

---
+++ musicas/src/main/java/br/senac/tads/dsw/musicas/MusicasApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.musicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicasApplication.class, args);
+	}
+
+}
